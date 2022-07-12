5d7a25daeaae9ee6de8a668c756d27cf5d075ae3
chris <chrishethertonweb@gmail.com>
2022-07-12 17:43:31 +0200

    exercise 2 done

--- solution.js
+++ solution.js
@@ -1,5 +1,14 @@
 let i = 0;
 while (i < 15) {
-  console.log(i);
   i++;
+  console.log(i);
 }
+console.log("HERE-----------------");
+
+let newVal = 1;
+let counter = 0;
+while (counter < 20) {
+  counter++;
+  newVal += +counter;
+}
+console.log(newVal);
